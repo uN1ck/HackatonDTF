9f9e3fd9811de1303ee9f4f2e7423f6af1542945
Siberian Barbarian <qserxsdiv@gmail.com>
2020-07-01 13:32:19 +0700

    Initial commit

---
+++ Assets/core/CommonGear.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public sealed class CommonGear : MonoBehaviour
+{
+    private Transform _transform;
+    private GearsController _gearsController;
+    private Action<Collision> _gearCollideHandler;
+
+    public static GameObject Instantiate(Transform parent, Action<Collision> gearCollideHandler)
+    {
+        var prefab = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/CommonGear.prefab", typeof(GameObject));
+        var newGear = Instantiate(prefab, Vector3.zero, Quaternion.identity, parent) as GameObject;
+        CommonGear gear = newGear.GetComponent<CommonGear>();
+        gear._gearCollideHandler = gearCollideHandler;
+        return newGear;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _transform = gameObject.transform;
+        _gearsController = _transform.GetComponentInParent<GearsController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        _gearCollideHandler.Invoke(other);
+    }
+}
---
+++ Assets/core/GearsController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GearsController : MonoBehaviour
+{
+    private List<CommonGear> _gears = new List<CommonGear>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        Object prefab = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/CommonGear.prefab", typeof(GameObject));
+        
+        for (int i = 0; i < 3; i++)
+        {
+            var newGear = Instantiate(prefab, Vector3.zero, Quaternion.identity, transform) as GameObject;
+            if (newGear != null)
+            {
+                newGear.transform.position += Vector3.one * i;
+                CommonGear gear = newGear.GetComponent<CommonGear>();
+                _gears.Add(gear);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
